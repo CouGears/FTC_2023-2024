4b85ef9b2be1376ffae77c0c25d542ea17019d4c
cmb2711 <45180163+cmb2711@users.noreply.github.com>
2023-10-30 19:53:52 -0400

    Caleb started CompetitionDriving2024.java and it theoreitcally should work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionDriving2024.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.SensorSet.LEDMethods;
+
+//TODO: Uncomment the following line to use
+@TeleOp
+public class CompetitionDriving2024 extends LinearOpMode {
+
+    public static DcMotor motorBR, motorBL, motorFL, motorFR, BackIntake, MiddleIntake;
+    public static Servo IntakeString;
+    private AutonMethods robot = new AutonMethods();
+    public int driveswitch = 1;
+
+    public int intakemode = 0;
+    private double botR = 1;
+    private double topR = 0;
+    private double botL = .35;
+    private double topL = 0;
+    private double right = 0;
+    private double left = 0;
+
+    @Override
+    public void runOpMode() {
+        //region hardware map
+        LEDMethods LED = new LEDMethods();
+        motorFL = hardwareMap.get(DcMotor.class, "motorFL");
+        motorBL = hardwareMap.get(DcMotor.class, "motorBL");
+        motorBR = hardwareMap.get(DcMotor.class, "motorBR");
+        motorFR = hardwareMap.get(DcMotor.class, "motorFR");
+        BackIntake = hardwareMap.get(DcMotor.class, "BackIntake");
+        MiddleIntake = hardwareMap.get(DcMotor.class, "MiddleIntake");
+
+        IntakeString = hardwareMap.get(Servo.class, "IntakeString");
+
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MiddleIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+        MiddleIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double speed = 1;
+            if (driveswitch == 0) {
+                speed = 1;
+            } else if (driveswitch == 1) {
+                speed = .66;
+            } else if (driveswitch == 2) {
+                speed = .333;
+            }
+
+            if (gamepad1.a && driveswitch<2) {
+                driveswitch +=1;
+            }
+            else if (gamepad1.b && driveswitch>-1) {
+                driveswitch -=1;
+            }
+
+            if (gamepad1.dpad_up && intakemode<1) {
+                intakemode +=1;
+            }
+            else if (gamepad1.dpad_down && driveswitch>-1) {
+                intakemode -=1;
+            }
+
+
+            motorFL.setPower(((this.gamepad1.right_stick_y) - (this.gamepad1.right_stick_x) + ((this.gamepad1.left_stick_y)) - (this.gamepad1.left_stick_x)) * speed);
+            motorBL.setPower(-(-(this.gamepad1.right_stick_y) + (this.gamepad1.right_stick_x) - (this.gamepad1.left_stick_y) - (this.gamepad1.left_stick_x)) * speed);
+            motorBR.setPower((-(this.gamepad1.right_stick_y) - (this.gamepad1.right_stick_x) - (this.gamepad1.left_stick_y) + (this.gamepad1.left_stick_x)) * speed);
+            motorFR.setPower(-((this.gamepad1.right_stick_y) + (this.gamepad1.right_stick_x) + (this.gamepad1.left_stick_y) + (this.gamepad1.left_stick_x)) * speed);
+
+
+            BackIntake.setPower(intakemode);
+            MiddleIntake.setPower(intakemode);
+
+            if (gamepad1.right_bumper) {
+                IntakeString.setPosition(1);
+            } else if (gamepad1.left_bumper) {
+                IntakeString.setPosition(0);
+            }
+        }
+    }
+}
+
+
+
+
+
