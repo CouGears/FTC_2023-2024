c4ee856eb3518ddb281b4dddaad4e9a260f64fa1
cmb2711 <45180163+cmb2711@users.noreply.github.com>
2023-11-21 09:18:33 -0500

    Worked on new robot changes. added drop servo. got rid of the *.66 on driving and made some changes to bumper and trigger controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionDriving2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionDriving2024.java
@@ -15,8 +15,9 @@ public class CompetitionDriving2024 extends LinearOpMode {
 
     public static DcMotor motorBR, motorBL, motorFL, motorFR, BackIntake, MiddleIntake, Lift;
     public static CRServo IntakeString;
+    public static Servo DropServo;
     private AutonMethods robot = new AutonMethods();
-    public int driveswitch = 1;
+    public int driveswitch = 2;
 
     public int intakemode = 0;
     private int liftLimit = 5500;
@@ -27,8 +28,10 @@ public class CompetitionDriving2024 extends LinearOpMode {
         telemetry.addData("Intake Mode", intakemode);
         telemetry.addLine();
         telemetry.addData("Servo Position", IntakeString.getPower());
-        telemetry.update();
-
+        telemetry.addLine();
+        telemetry.addData("Lift Pos = ", Lift.getCurrentPosition());
+        telemetry.addLine();
+        telemetry.addData("Drop Servo Pos = ", DropServo.getPosition());
         telemetry.update();
     }
     @Override
@@ -41,8 +44,10 @@ public class CompetitionDriving2024 extends LinearOpMode {
         motorFR = hardwareMap.get(DcMotor.class, "motorFR");
         BackIntake = hardwareMap.get(DcMotor.class, "BackIntake");
         MiddleIntake = hardwareMap.get(DcMotor.class, "MiddleIntake");
-
+        Lift = hardwareMap.get(DcMotor.class, "Lift");
         IntakeString = hardwareMap.get(CRServo.class, "IntakeString");
+
+        DropServo = hardwareMap.get(Servo.class, "DropServo");
 
         motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -50,6 +55,8 @@ public class CompetitionDriving2024 extends LinearOpMode {
         motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         MiddleIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -85,47 +92,51 @@ public class CompetitionDriving2024 extends LinearOpMode {
                 driveswitch -=1;
             }
 
-            if (gamepad1.dpad_up && intakemode<1) {
+            if (gamepad1.right_bumper && intakemode<1) {
                 intakemode +=1;
             }
-            else if (gamepad1.dpad_down && intakemode>-1) {
+            else if (gamepad1.left_bumper && intakemode>-1) {
                 intakemode -=1;
             }
-            else if (gamepad1.dpad_left) {
+            else if (gamepad1.right_trigger>.5||gamepad1.left_trigger>.5) {
                 intakemode = 0;
                 driveswitch = 1;
             }
 
 
-            motorFL.setPower(((this.gamepad1.right_stick_y) - (this.gamepad1.right_stick_x) + ((this.gamepad1.left_stick_y)) - (this.gamepad1.left_stick_x)) * speed*.67);
+            motorFL.setPower(((this.gamepad1.right_stick_y) - (this.gamepad1.right_stick_x) + ((this.gamepad1.left_stick_y)) - (this.gamepad1.left_stick_x)) * speed);
             motorBL.setPower(-(-(this.gamepad1.right_stick_y) + (this.gamepad1.right_stick_x) - (this.gamepad1.left_stick_y) - (this.gamepad1.left_stick_x)) * speed);
             motorBR.setPower((-(this.gamepad1.right_stick_y) - (this.gamepad1.right_stick_x) - (this.gamepad1.left_stick_y) + (this.gamepad1.left_stick_x)) * speed);
-            motorFR.setPower(-((this.gamepad1.right_stick_y) + (this.gamepad1.right_stick_x) + (this.gamepad1.left_stick_y) + (this.gamepad1.left_stick_x)) * speed*.67);
+            motorFR.setPower(-((this.gamepad1.right_stick_y) + (this.gamepad1.right_stick_x) + (this.gamepad1.left_stick_y) + (this.gamepad1.left_stick_x)) * speed);
 
 
             BackIntake.setPower(intakemode);
             MiddleIntake.setPower(-1*intakemode);
-            if (gamepad1.right_bumper) {
-                IntakeString.setPower(0.5);
+            if (gamepad1.right_trigger>.1) {
+                IntakeString.setPower(gamepad1.right_trigger);
             }
-            else if (gamepad1.left_bumper) {
-                IntakeString.setPower(-0.5);
+            else if (gamepad1.left_trigger>.1) {
+                IntakeString.setPower(-1*gamepad1.left_trigger);
             }
             else {
                 IntakeString.setPower(0.0);
             }
 
             //LIFT
-            if (gamepad1.dpad_up && Lift.getCurrentPosition() <= liftLimit) {
+            if ((gamepad1.dpad_up && Lift.getCurrentPosition() <= liftLimit) || (gamepad1.dpad_up && gamepad1.dpad_right)) {
                 Lift.setPower(1);
-            } else if (gamepad1.dpad_down && Lift.getCurrentPosition() >= 500){ //At 500 b/c motor will overspin w/ momentum and end up <0
+            } else if ((gamepad1.dpad_down && Lift.getCurrentPosition() >= 500) || (gamepad1.dpad_down && gamepad1.dpad_right)){ //At 500 b/c motor will overspin w/ momentum and end up <0
                 Lift.setPower(-1);
             } else {
                 Lift.setPower(0);
             }
+            if (gamepad1.dpad_left) {
+                DropServo.setPosition(0);
+            }
+            else{
+                DropServo.setPosition(0.5);
+            }
 
-            telemetry.addData("Lift Pos = ", Lift.getCurrentPosition());
-            telemetry.update();
             }
         }
     }
